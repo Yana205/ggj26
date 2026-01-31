260b34590cb665e35071e61fc96f2d011ac44a6e
Yana Ifraimov <yanaifraim@gmail.com>
2026-01-31 17:28:46 +0200

    Deleted obsolete debugging

--- Assets/scripts/GrandmaSpeechBubble.cs
+++ Assets/scripts/GrandmaSpeechBubble.cs
@@ -77,7 +77,6 @@ public class GrandmaSpeechBubble : MonoBehaviour
         if (bubbleContainer != null)
         {
             bubbleContainer.transform.position = transform.position + bubbleOffset;
-            Debug.Log($"Bubble location {bubbleContainer.transform.position.x}, {bubbleContainer.transform.position.y}");
         }
     }
 
